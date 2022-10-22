b45ca3d6036c1ead026dd047287f445a1dd16642
Ezequiel Carrasco <ezequielcarrasco96@gmail.com>
2022-10-22 18:22:37 -0300

    cargaEntregaSprint2

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-const path = require('path');
-const publicPath = path.join(__dirname, './public'); 
-app.use(express.static(publicPath));
-
-app.listen(3090, ()=> console.log('Servidor OK - THE NFT MARKET - 3090'));
