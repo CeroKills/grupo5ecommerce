60d1ce675bafb0d83c369cee8622c845b61b1f18
joadiaz02 <joacodiaz02@hotmail.com>
2022-10-28 01:09:40 -0300

    includes creados

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+let ejs = require('ejs');
 const rutasHome = require('./routers/home.js');
 const rutasCarrito = require('./routers/carrito.js');
 const rutasLogin = require('./routers/login.js');
